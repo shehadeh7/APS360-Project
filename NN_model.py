8197390e6c939608f6a470db4c568eb33a456890
shehadeh7 <51853688+shehadeh7@users.noreply.github.com>
2022-02-16 00:43:46 -0500

    Add comment line for nsl install

--- NN_model.py
+++ NN_model.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
 import neural_structured_learning as nsl
 
+'''
+If running in google colab, make sure you run this command first:
+
+!pip install neural-structured-learning
+
+'''
+
 # Prepare data.
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
